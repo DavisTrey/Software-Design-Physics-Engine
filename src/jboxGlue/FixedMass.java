56cc69cd33e0510c80e027249ae6f2e6d6d4c8ec
Jacob Lettie <jrl47@duke.edu>
2014-02-01 15:44:59 -0500

    Modified FixedMass constructor to use default mass instead.

--- src/jboxGlue/FixedMass.java
+++ src/jboxGlue/FixedMass.java
@@ -3,9 +3,10 @@ package jboxGlue;
 import jgame.JGColor;
 
 public class FixedMass extends PhysicalObjectCircle {
-	
-	public FixedMass(String id, double x, double y, double mass){
-		super(id, 1, JGColor.red, 10, mass);
+	private static final double DEFAULT_MASS = 1;
+	private static final double DEFAULT_RADIUS = 10;
+	public FixedMass(String id, double x, double y){
+		super(id, 1, JGColor.red, DEFAULT_RADIUS, DEFAULT_MASS);
 		this.setPos(x, y);
 	}
 	public void move(){
