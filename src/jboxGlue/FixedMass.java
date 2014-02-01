1870efd673937ca3c530baf940f34e747338c245
Jacob Lettie <jrl47@duke.edu>
2014-02-01 15:38:29 -0500

    Modified constructor parameters for FixedMass and overrode move() method.

--- src/jboxGlue/FixedMass.java
+++ src/jboxGlue/FixedMass.java
@@ -4,9 +4,11 @@ import jgame.JGColor;
 
 public class FixedMass extends PhysicalObjectCircle {
 	
-	public FixedMass(String id, int x, int y, int mass){
+	public FixedMass(String id, double x, double y, double mass){
 		super(id, 1, JGColor.red, 10, mass);
 		this.setPos(x, y);
 	}
-
+	public void move(){
+		
+	}
 }
