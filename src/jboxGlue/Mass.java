739aa0ddb0141ec0ff62c0e04b9502840fb0f974
Jacob Lettie <jrl47@duke.edu>
2014-02-01 15:52:32 -0500

    Implemented a Mass object.

---
+++ src/jboxGlue/Mass.java
@@ -0,0 +1,23 @@
+package jboxGlue;
+
+import org.jbox2d.common.Vec2;
+
+import jgame.JGColor;
+
+public class Mass extends PhysicalObjectCircle {
+	private static final double DEFAULT_RADIUS = 10;
+	public Mass(String id, double x, double y, double xvelo, double yvelo, double mass){
+		super(id, 1, JGColor.red, DEFAULT_RADIUS, mass);
+		this.setPos(x, y);
+		this.setVelo(xvelo, yvelo);
+	}
+	
+	public void setVelo(double x, double y){
+        // there's no body while the game object is initializing
+        if (myBody != null) { 
+            // set velocity of jbox2d object, not jgame object
+        	myBody.setLinearVelocity(new Vec2((float)x, (float)y));
+        }
+	}
+
+}
