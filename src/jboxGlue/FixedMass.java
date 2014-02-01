f97b4a13aa57c6f4ff53eb0b10498fe39f75609c
Jacob Lettie <jrl47@duke.edu>
2014-02-01 16:56:06 -0500

    Committed changes to fixedmass for pull.

--- src/jboxGlue/FixedMass.java
+++ src/jboxGlue/FixedMass.java
@@ -1,21 +1,13 @@
 package jboxGlue;
 
 import jgame.JGColor;
-import jboxGlue.PhysicalObject;
 import jboxGlue.PhysicalObjectCircle;
-import jboxGlue.PhysicalObjectRect;
-import jboxGlue.WorldManager;
-import jgame.JGColor;
-import jgame.JGObject;
-import jgame.platform.JGEngine;
-import org.jbox2d.common.Vec2;
-
 
 public class FixedMass extends PhysicalObjectCircle {
 	private static final double DEFAULT_MASS = 1;
 	private static final double DEFAULT_RADIUS = 10;
 	public FixedMass(String id, double x, double y){
 		super(id, 1, JGColor.red, DEFAULT_RADIUS, DEFAULT_MASS);
-		this.setPos(100, 100);
+		this.setPos(x, y);
 	}
 }
