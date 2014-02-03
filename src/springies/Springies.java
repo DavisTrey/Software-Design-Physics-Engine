af19dc1de5156f84dfc268a9b633c47f4eadd540
Davis Treybig <davis.treybig@gmail.com>
2014-02-03 04:57:38 -0500

    Added environment XML reading

--- src/springies/Springies.java
+++ src/springies/Springies.java
@@ -1,9 +1,11 @@
 package springies;
 
+import java.awt.Frame;
 import java.io.File;
 import java.util.HashSet;
 
 import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -35,10 +37,10 @@ public class Springies extends JGEngine{
 	private static final String DEFAULT_MASS="1";
 	private static final String DEFAULT_REST="150";
 	public static final String DEFAULT_SPRINGCONSTANT="1";
-	private static final double WALL_FORCE_CONSTANT = 100000;
-	private static final double CENTEROFMASS_FORCE_CONSTANT = 50000;
-	private static double centerOfMassExponent = 2;
-	public static double[] wallForces = {2,2,2,2};
+	private static double CENTEROFMASS_FORCE_CONSTANT = 2000;
+	private static double CENTEROFMASS_EXPONENT = 2;
+	public static double[] wallForceExponents = {2,2,2,2};
+	private static final double WALL_FORCE_CONSTANT[] = {100000, 100000, 100000, 100000};
 	public static PhysicalObject[] walls = new PhysicalObject[4];
     public Springies (){
 
@@ -73,10 +75,15 @@ public class Springies extends JGEngine{
         WorldManager.getWorld().setGravity(new Vec2(0.0f, 0.1f));
         addWalls();
         readXMLData();
+        readEnvironmentData();
         
     }
+    
     public void readXMLData(){
 		try {
+			Frame message=new Frame();
+			JOptionPane.showMessageDialog(message, "Please load your object XML File");
+			
 			File dataFile = new File("");
 			JFileChooser chooser = new JFileChooser();
 			FileNameExtensionFilter filter = new FileNameExtensionFilter(
@@ -118,24 +125,15 @@ public class Springies extends JGEngine{
 					String id=element.getAttribute("id");
 					String xpos=element.getAttribute("x");
 					String ypos=element.getAttribute("y");
-					String xVeloc=null;
-					String yVeloc=null;
-					String mass=null;
-					try{
-						xVeloc=element.getAttribute("vx");
-						yVeloc=element.getAttribute("vy");
-					}
-					catch(Exception ex){
-						xVeloc=DEFAULT_VELOCITY;
-						yVeloc=DEFAULT_VELOCITY;
-					}
-					try{
-						mass=element.getAttribute("mass");
-					}
-					catch(Exception ex){
-					}
+					String xVeloc=element.getAttribute("vx");
+					String yVeloc=element.getAttribute("vy");
+					String mass=element.getAttribute("mass");
 					if(mass==""){
 						mass=DEFAULT_MASS;
+					}
+					if(yVeloc==""){
+						yVeloc=DEFAULT_VELOCITY;
+						xVeloc=DEFAULT_VELOCITY;
 					}
 					createMass(id, xpos, ypos, xVeloc, yVeloc, mass);
 					
@@ -157,32 +155,15 @@ public class Springies extends JGEngine{
 						Element element = (Element) node;
 						String id1=element.getAttribute("a");
 						String id2=element.getAttribute("b");
-						String restLength=null;
-						String springConstant=null;
-						String amplitude=null;
-						try{
-							restLength=element.getAttribute("restlength");
-						}
-						catch(Exception ex){
-						}
+						String restLength=element.getAttribute("restlength");
+						String springConstant=element.getAttribute("constant");
+						String amplitude=element.getAttribute("amplitude");
 						if(restLength==""){
 							restLength=DEFAULT_REST;
-						}
-						try{
-							springConstant=element.getAttribute("constant");
-						}
-						catch(Exception ex){
-							springConstant=DEFAULT_SPRINGCONSTANT;
 						}
 						if(springConstant==""){
 							springConstant=DEFAULT_SPRINGCONSTANT;
 						}
-						try{
-							amplitude=element.getAttribute("amplitude");
-						}
-						catch(Exception ex){
-						}
-						
 						if(amplitude!=""){
 							createMuscle(id1, id2, restLength, springConstant, amplitude);
 						}
@@ -204,7 +185,113 @@ public class Springies extends JGEngine{
 			ex.printStackTrace();
 		}
 	}
+    public void readEnvironmentData(){
+    	try {
+    		Frame message=new Frame();
+			Object[] options={"Yes", "No"};
+			int n=JOptionPane.showOptionDialog(message, "Would you like to load an enviroment file?", "XML Loading", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, options[1]);
+			if(n==0){
+				File dataFile = new File("");
+				JFileChooser chooser = new JFileChooser();
+				FileNameExtensionFilter filter = new FileNameExtensionFilter(
+				        "XML Files", "xml");
+				    chooser.setFileFilter(filter);
+				    int returnVal = chooser.showOpenDialog(getParent());
+				    if(returnVal == JFileChooser.APPROVE_OPTION) {
+						dataFile = chooser.getSelectedFile();
+				    }
+				DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+				DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+				Document doc = dBuilder.parse(dataFile);
+				doc.getDocumentElement().normalize();
+				//Gravity
+				NodeList nodes = doc.getElementsByTagName("gravity");	
+				for (int i = 0; i < nodes.getLength(); i++) {
+					Node node = nodes.item(i);
 
+					if (node.getNodeType() == Node.ELEMENT_NODE) {
+						Element element = (Element) node;
+						String direction=element.getAttribute("direction");
+						String magnitude=element.getAttribute("magnitude");
+						System.out.println("Gravity--------");
+						System.out.println("Direction:" + direction);
+						System.out.println("Mag:" + magnitude);
+						alterGravity(direction, magnitude);
+					}
+				}
+				//Viscosity
+				nodes=doc.getElementsByTagName("viscosity");
+				for (int i = 0; i < nodes.getLength(); i++) {
+					Node node = nodes.item(i);
+					if (node.getNodeType() == Node.ELEMENT_NODE) {
+						Element element = (Element) node;
+						String magnitude=element.getAttribute("magnitude");
+						System.out.println("Viscosity--------");
+						System.out.println("Mag:" + magnitude);
+						alterViscosity(magnitude);
+					}
+				}
+				
+				//centermass
+				nodes=doc.getElementsByTagName("centermass");
+				for (int i = 0; i < nodes.getLength(); i++) {
+					Node node = nodes.item(i);
+					if (node.getNodeType() == Node.ELEMENT_NODE) {
+						Element element = (Element) node;
+						String magnitude=element.getAttribute("magnitude");
+						String exponent=element.getAttribute("exponent");
+						System.out.println("Center--------");
+						System.out.println("Mag:" + magnitude);
+						System.out.println("Exponent:" + exponent);
+						alterCenterMass(magnitude, exponent);
+					}
+				}
+				
+				//Walls
+				nodes=doc.getElementsByTagName("wall");
+				for (int i = 0; i < nodes.getLength(); i++) {
+					Node node = nodes.item(i);
+					if (node.getNodeType() == Node.ELEMENT_NODE) {
+						Element element = (Element) node;
+						String id=element.getAttribute("id");
+						String magnitude=element.getAttribute("magnitude");
+						String exponent=element.getAttribute("exponent");
+						System.out.println("Wall--------");
+						System.out.println("Mag:" + magnitude);
+						System.out.println("Exponent:" + exponent);
+						System.out.println("Id: " + id);
+						alterWall(id, magnitude, exponent);
+					}
+				}
+			}
+			
+
+
+    	} catch (Exception ex) {
+    		ex.printStackTrace();
+    	}
+    }
+    public void alterGravity(String direction, String magnitude){
+    	double direct=Double.parseDouble(direction);
+    	double mag=Double.parseDouble(magnitude);
+    	Vec2 vector=new Vec2((float)(mag*Math.cos(direct*3.14/180)),(float)(mag*Math.sin(direct*3.14/180)));
+    	WorldManager.getWorld().setGravity(vector);
+    }
+    public void alterViscosity(String magnitude){
+    	viscosity=Double.parseDouble(magnitude);
+    }
+    public void alterCenterMass(String magnitude, String exponent){
+    	CENTEROFMASS_FORCE_CONSTANT=Double.parseDouble(magnitude);
+    	CENTEROFMASS_EXPONENT=Double.parseDouble(exponent);	
+    }
+    public void alterWall(String id, String magnitude, String exponent){
+    	int wallIndex=(int)(Double.parseDouble(id)-1);
+    	double mag=Double.parseDouble(magnitude);
+    	double exp=Double.parseDouble(exponent);
+    	wallForceExponents[wallIndex]=exp; 
+    	WALL_FORCE_CONSTANT[wallIndex]=mag;
+    	
+    }
 	public void createMuscle(String id1, String id2, String rest, String K, String amp){
 		double restLength=Double.parseDouble(rest);
 		double springConstant=Double.parseDouble(K);
@@ -276,9 +363,9 @@ public class Springies extends JGEngine{
 		double distance = Math.sqrt(Math.pow(xComp, 2)+Math.pow(yComp, 2));
 		xComp = xComp/distance;
 		yComp = yComp/distance;
-		double magnitude = CENTEROFMASS_FORCE_CONSTANT/Math.pow(distance, Math.abs(centerOfMassExponent));
+		double magnitude = CENTEROFMASS_FORCE_CONSTANT/Math.pow(distance, Math.abs(CENTEROFMASS_EXPONENT));
 		Vec2 comForce = new Vec2((float)(magnitude*xComp),(float)(magnitude*yComp));
-		if(centerOfMassExponent<0){
+		if(CENTEROFMASS_EXPONENT<0){
 			b.applyForce(comForce.negate(), b.m_xf.position);
 		}
 		else{
@@ -295,10 +382,11 @@ public class Springies extends JGEngine{
     	}
     }
 	private void applyWallForce(Body b) {
-		b.applyForce(new Vec2((float)0,(float)(WALL_FORCE_CONSTANT/(Math.pow(Math.abs(b.m_xf.position.y-walls[0].y),wallForces[0])))), b.m_xf.position);
-		b.applyForce(new Vec2((float)((-1)*WALL_FORCE_CONSTANT/(Math.pow(Math.abs(b.m_xf.position.x-walls[1].x),wallForces[1]))), (float)0), b.m_xf.position);
-		b.applyForce(new Vec2((float)0,(float)((-1)*WALL_FORCE_CONSTANT/(Math.pow(Math.abs(b.m_xf.position.y-walls[2].y),wallForces[2])))), b.m_xf.position);
-		b.applyForce(new Vec2((float)(WALL_FORCE_CONSTANT/(Math.pow(Math.abs(b.m_xf.position.x-walls[3].x),wallForces[3]))), (float)0), b.m_xf.position);
+	
+		b.applyForce(new Vec2((float)0,(float)(WALL_FORCE_CONSTANT[0]/(Math.pow(Math.abs(b.m_xf.position.y-walls[0].y),wallForceExponents[0])))), b.m_xf.position);
+		b.applyForce(new Vec2((float)((-1)*WALL_FORCE_CONSTANT[1]/(Math.pow(Math.abs(b.m_xf.position.x-walls[1].x),wallForceExponents[1]))), (float)0), b.m_xf.position);
+		b.applyForce(new Vec2((float)0,(float)((-1)*WALL_FORCE_CONSTANT[2]/(Math.pow(Math.abs(b.m_xf.position.y-walls[2].y),wallForceExponents[2])))), b.m_xf.position);
+		b.applyForce(new Vec2((float)(WALL_FORCE_CONSTANT[3]/(Math.pow(Math.abs(b.m_xf.position.x-walls[3].x),wallForceExponents[3]))), (float)0), b.m_xf.position);
 	}
 
 	private void applyViscosity(Body b) {
