4a061cb1fc903f5defd0a6dce4f3e1d9ccd26f63
Davis Treybig <davis.treybig@gmail.com>
2014-02-07 04:16:02 -0500

    Small typo

--- src/springies/Springies.java
+++ src/springies/Springies.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
+import XML.*;
 
 import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
@@ -50,11 +51,12 @@ public class Springies extends JGEngine{
 	private static final double[] DEFAULT_WALL_FORCE_EXPONENTS = {2,2,2,2};
 	private static final double DEFAULT_WALL_FORCE_CONSTANT[] = {100000, 100000, 100000, 100000};
 	private Vec2 centerOfMass = new Vec2(0,0);
-	private static PhysicalObject[] walls = new PhysicalObject[4];
+	protected static PhysicalObject[] walls = new PhysicalObject[4];
 	private int wallModifier = 0;
-	private int assemblyNumber = 0;
+	protected int assemblyNumber = 0;
 	// Forces are indexed as follows: 0=gravity, 1=viscosity, 2=centerofmass, 3=top wall, 4=right wall, 5=bottom wall, 6=left wall
-	private static Force[] forces = new Force[7];
+	protected static Force[] forces = new Force[7];
+	
 	
     public Springies (){
 
@@ -92,193 +94,75 @@ public class Springies extends JGEngine{
         XMLMessage();
         
     }
+  
     public void XMLMessage(){
 		try {
 			Frame message=new Frame();
 			JOptionPane.showMessageDialog(message, "Please load your object XML File");
-			
-			File dataFile = new File("");
-			JFileChooser chooser = new JFileChooser();
-			FileNameExtensionFilter filter = new FileNameExtensionFilter(
-			        "XML Files", "xml");
-			    chooser.setFileFilter(filter);
-			    int returnVal = chooser.showOpenDialog(getParent());
-			    if(returnVal == JFileChooser.APPROVE_OPTION) {
-					dataFile = chooser.getSelectedFile();
-			    }
-			    
-			readXMLData(dataFile);
+			File dataFile=fileRetriever();
+			readObjectData(dataFile);
 
 			message=new Frame();
 			Object[] options={"Yes", "No"};
 			int n=JOptionPane.showOptionDialog(message, "Would you like to load an enviroment file?", "XML Loading", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, options[1]);
 			if(n==0){
-				dataFile = new File("");
-				chooser = new JFileChooser();
-				filter = new FileNameExtensionFilter(
-				        "XML Files", "xml");
-				    chooser.setFileFilter(filter);
-				    returnVal = chooser.showOpenDialog(getParent());
-				    if(returnVal == JFileChooser.APPROVE_OPTION) {
-						dataFile = chooser.getSelectedFile();
-				    }
+					dataFile=fileRetriever();
 				    readEnvironmentData(dataFile);
 			}
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
 	}
-	public void readXMLData(File dataFile){
+    public File fileRetriever(){
+    	File dataFile = new File("");
+		JFileChooser chooser = new JFileChooser();
+		FileNameExtensionFilter filter = new FileNameExtensionFilter(
+		        "XML Files", "xml");
+		    chooser.setFileFilter(filter);
+		    int returnVal = chooser.showOpenDialog(getParent());
+		    if(returnVal == JFileChooser.APPROVE_OPTION) {
+				dataFile = chooser.getSelectedFile();
+		    }
+		    return dataFile;
+    }
+    
+    public Document DocumentBuilder(File dataFile){
+    	try{
+    		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+    		DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+    		Document doc = dBuilder.parse(dataFile);
+    		doc.getDocumentElement().normalize();	
+    		return doc;
+    	} catch (Exception ex) {
+		ex.printStackTrace();
+	}
+    	return null; //To fix Eclipse error with returns in try{}
+    }
+   
+	public void readObjectData(File dataFile){
 		try {
-			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-			Document doc = dBuilder.parse(dataFile);
-			doc.getDocumentElement().normalize();
-
-			//Reading Fixed Masses
-			NodeList nodes = doc.getElementsByTagName("fixed");	
-			for (int i = 0; i < nodes.getLength(); i++) {
-				Node node = nodes.item(i);
-
-				if (node.getNodeType() == Node.ELEMENT_NODE) {
-					Element element = (Element) node;
-					String id=element.getAttribute("id");
-					String xpos=element.getAttribute("x");
-					String ypos=element.getAttribute("y");
-					createFixed(id, xpos, ypos);
+			Document doc=DocumentBuilder(dataFile);
+			new FixedReader().readNodes("fixed", doc, this);
+			new NormalMassReader().readNodes("mass", doc, this);
+			new SpringAndMuscleReader().readNodes("spring", doc, this);
+			new SpringAndMuscleReader().readNodes("muscle", doc, this);
 					
-					//System.out.println("ID: " + id);
-					//System.out.println("X Position: " + xpos);
-					//System.out.println("Y Position: " + ypos);
-					
-				}
-			}
-			//Reading Regular Masses
-			nodes=doc.getElementsByTagName("mass");
-			for(int i=0; i <nodes.getLength(); i++){
-				Node node=nodes.item(i);
-				if(node.getNodeType()==Node.ELEMENT_NODE){
-					Element element = (Element) node;
-					String id=element.getAttribute("id");
-					String xpos=element.getAttribute("x");
-					String ypos=element.getAttribute("y");
-					String xVeloc=element.getAttribute("vx");
-					String yVeloc=element.getAttribute("vy");
-					String mass=element.getAttribute("mass");
-					if(mass==""){
-						mass=DEFAULT_MASS;
-					}
-					if(yVeloc==""){
-						yVeloc=DEFAULT_VELOCITY;
-						xVeloc=DEFAULT_VELOCITY;
-					}
-					createMass(id, xpos, ypos, xVeloc, yVeloc, mass);
-					
-					//System.out.println("ID: " + id);
-					//System.out.println("X Position: " + xpos);
-					//System.out.println("Y Position: " + ypos);
-					//System.out.println("X Velocity: "+ xVeloc);
-					//System.out.println("Y Velocity: "+ yVeloc);
-					//System.out.println("Mass: " + mass);
-				}
-			}
-			//Reading Springs and muscles
-			String[] array={"spring", "muscle"};
-			for(String s: array){
-				nodes=doc.getElementsByTagName(s);
-				for(int i=0; i <nodes.getLength(); i++){
-					Node node=nodes.item(i);
-					if(node.getNodeType()==Node.ELEMENT_NODE){
-						Element element = (Element) node;
-						String id1=element.getAttribute("a");
-						String id2=element.getAttribute("b");
-						String restLength=element.getAttribute("restlength");
-						String springConstant=element.getAttribute("constant");
-						String amplitude=element.getAttribute("amplitude");
-						if(restLength==""){
-							restLength=DEFAULT_REST;
-						}
-						if(springConstant==""){
-							springConstant=DEFAULT_SPRINGCONSTANT;
-						}
-						if(amplitude!=""){
-							createMuscle(id1, id2, restLength, springConstant, amplitude);
-						}
-						else{
-							createSpring(id1, id2, restLength, springConstant);
-						}
-						
-						//System.out.println("id1: " + id1);
-						//System.out.println("id2: " + id2);
-						//System.out.println("Rest Length: " + restLength);
-						//System.out.println("K: " + springConstant);
-					}
-			}
-			}
-				
-			
-			
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
 	}
-    public void readEnvironmentData(File dataFile){
-    	try {
-				DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-				DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-				Document doc = dBuilder.parse(dataFile);
-				doc.getDocumentElement().normalize();
-				//Gravity
-				NodeList nodes = doc.getElementsByTagName("gravity");	
-				for (int i = 0; i < nodes.getLength(); i++) {
-					Node node = nodes.item(i);
+	public void readEnvironmentData(File dataFile){
+		try {
+			Document doc=DocumentBuilder(dataFile);
+			new GravityReader().readNodes("gravity", doc, this);
+			new ViscosityReader().readNodes("viscosity", doc, this);
+			new CenterOfMassReader().readNodes("centermass", doc, this);
+			new WallReader().readNodes("wall", doc, this);
 
-					if (node.getNodeType() == Node.ELEMENT_NODE) {
-						Element element = (Element) node;
-						String direction=element.getAttribute("direction");
-						String magnitude=element.getAttribute("magnitude");
-						alterGravity(direction, magnitude);
-					}
-				}
-				//Viscosity
-				nodes=doc.getElementsByTagName("viscosity");
-				for (int i = 0; i < nodes.getLength(); i++) {
-					Node node = nodes.item(i);
-					if (node.getNodeType() == Node.ELEMENT_NODE) {
-						Element element = (Element) node;
-						String magnitude=element.getAttribute("magnitude");
-						alterViscosity(magnitude);
-					}
-				}
-				
-				//centermass
-				nodes=doc.getElementsByTagName("centermass");
-				for (int i = 0; i < nodes.getLength(); i++) {
-					Node node = nodes.item(i);
-					if (node.getNodeType() == Node.ELEMENT_NODE) {
-						Element element = (Element) node;
-						String magnitude=element.getAttribute("magnitude");
-						String exponent=element.getAttribute("exponent");
-						alterCenterMass(magnitude, exponent);
-					}
-				}
-				
-				//Walls
-				nodes=doc.getElementsByTagName("wall");
-				for (int i = 0; i < nodes.getLength(); i++) {
-					Node node = nodes.item(i);
-					if (node.getNodeType() == Node.ELEMENT_NODE) {
-						Element element = (Element) node;
-						String id=element.getAttribute("id");
-						String magnitude=element.getAttribute("magnitude");
-						String exponent=element.getAttribute("exponent");
-						alterWall(id, magnitude, exponent);
-					}
-				}
-    	} catch (Exception ex) {
-    		ex.printStackTrace();
-    	}
-    }
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
     public void alterGravity(String direction, String magnitude){
     	double direct=Double.parseDouble(direction);
     	double mag=Double.parseDouble(magnitude);
@@ -324,6 +208,8 @@ public class Springies extends JGEngine{
 		new FixedMass(id, xPosition, yPosition, assemblyNumber);
 	
 	}
+	
+
 	private void addWalls ()
     {
         // add walls to bounce off of
